0891ef51a53dae00e8ad7593cfd72e76fd82098b
Koki Kubo <c11h17n6o8@gmail.com>
2025-05-20 22:41:05 +0900

    Change to HashMap

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,43 @@
 use actix_web::{App, HttpResponse, HttpServer, Responder, web};
+use std::collections::HashMap;
 use std::sync::{Arc, Mutex};
 use tokio::time::{Duration, sleep};
 
 #[derive(Clone)]
 struct AppState {
-    counter: Arc<Mutex<i32>>,
+    map: Arc<Mutex<HashMap<String, i32>>>,
 }
 
 async fn index(data: web::Data<AppState>) -> impl Responder {
-    let mut counter = data.counter.lock().unwrap();
+    let mut map = data.map.lock().unwrap();
+    let counter = map.entry("counter".to_string()).or_insert(0);
     *counter += 1;
     HttpResponse::Ok().body(format!("Counter: {}", counter))
+}
+
+async fn monitor_state(state: Arc<Mutex<HashMap<String, i32>>>) {
+    loop {
+        {
+            let map = state.lock().unwrap();
+            if let Some(counter) = map.get("counter") {
+                println!("Current Counter: {}", counter);
+            } else {
+                println!("Counter not found");
+            }
+        }
+        sleep(Duration::from_secs(5)).await; // 5秒ごとに状態を監視
+    }
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     let state = AppState {
-        counter: Arc::new(Mutex::new(0)),
+        map: Arc::new(Mutex::new(HashMap::new())),
     };
 
     // クローンされた状態で監視タスクを生成
-    let state_clone = state.clone();
-    tokio::spawn(async move {
-        loop {
-            {
-                let counter = state_clone.counter.lock().unwrap();
-                println!("Current Counter: {}", counter);
-            }
-            sleep(Duration::from_secs(5)).await; // 5秒ごとに状態を監視
-        }
-    });
+    let state_clone = state.map.clone();
+    tokio::spawn(monitor_state(state_clone));
 
     // HTTPサーバの起動
     HttpServer::new(move || {
